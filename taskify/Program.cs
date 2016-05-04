a88e457353e4001569ab981307d66f9fe1b7d8c9
michael berezin <mbearz@gmail.com>
2016-05-04 20:19:22 +0300

    first checkin

---
+++ taskify/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace taskify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<User> {
+                    new User { Id=1, Name="michael"},
+                    new User { Id=2, Name="edi"},
+                    new User { Id=3, Name="alex"},
+                    new User { Id=4, Name="avi"},
+                    new User { Id=5, Name="yoni"}
+                };
+            Task.Run(async () =>
+            {
+                Stopwatch start = new Stopwatch();
+                start.Start();
+
+                List<Task<int>> taskRegular = list.Select(u =>
+                Task.Run(() =>
+                {
+                    Thread.Sleep(3000);
+                    return u.Id - 5;
+                })).ToList();
+
+                await Task.WhenAll(taskRegular);
+                var firstRegular = taskRegular[0].Result;
+
+                start.Stop();
+
+                Console.WriteLine("regular time: " + start.Elapsed.TotalSeconds + " answer " + firstRegular);
+
+                start.Reset();
+                start.Start();
+                var taskExt = await list.ToTask(u =>
+                {
+                    Thread.Sleep(3000);
+                    return u.Id - 5;
+                }).WhenAllAsync();
+
+                var firstExt = taskExt[0];
+                start.Stop();
+                Console.WriteLine("regular time: " + start.Elapsed.TotalSeconds + " answer " + firstExt);
+                Console.Read();
+            }).Wait();
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ taskify/TaskExtentions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskify
+{
+    public static class TaskExtentions
+    {
+        public static List<Task<TO>> ToTask<T,TO> (this IEnumerable<T> source, Func<T, TO> func)
+        {
+            if(!source.Any())
+            {
+                throw new InvalidOperationException("Cannot create list of an empty list.");
+            }
+
+            return source.Select(item=> Task.Run(() => func(item))).ToList();
+        }
+
+        public static TO[] WhenAll<TO>(this IEnumerable<Task<TO>> source)
+        {
+            return Task.WhenAll(source).Result;
+        }
+
+        public async static Task<TO[]> WhenAllAsync<TO>(this IEnumerable<Task<TO>> source)
+        {
+            return await Task.WhenAll(source);
+        }
+    }
+}
